ab438a1cd96b340bc7af897c890c60e910d510e3
sunjing <beiyouok@dtstack.com>
2025-03-08 02:32:02 +0800

    feat: build a app

--- README.md
+++ README.md
@@ -10,3 +10,13 @@ cd ../
 rmdir SpiritApp
 强制删除
 rm -rf SpiritApp
+
+安装vscode插件
+expo tools 
+ES7+ React/Redux/React-Native snippets 
+rnfe 函数式组件
+Commond(Ctrl) + D 快速选中重复名称
+
+#2. feat: build a app
+创建 declarations.d.ts 解决 TypeScript 识别问题
+Shift + Option + ⬇️ 快速向下复制
---
+++ app/index.tsx
@@ -0,0 +1,74 @@
+import { View, Text, StyleSheet, ImageBackground, Pressable } from 'react-native'
+import React from 'react'
+
+import icedCofeeImg from '@/assets/images/iced-coffee.png';
+import { Link } from 'expo-router';
+
+const app = () => {
+  return (
+    <View style={styles.container}>
+      <ImageBackground
+        source={icedCofeeImg}
+        resizeMode='cover'
+        style={styles.image}
+      >
+        <Text style={styles.text}>Coffee Shop</Text>
+
+        <Link style={styles.link} href="/explore" asChild>
+          <Pressable style={styles.button}>
+            <Text style={styles.buttonText}>
+              Explore
+            </Text>
+          </Pressable>
+        </Link>
+      </ImageBackground>
+    </View>
+  )
+}
+
+export default app
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+  },
+  text: {
+    color: 'white',
+    fontSize: 42,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    backgroundColor: 'rgba(0,0,0,.5)',
+    marginBottom: 120,
+  },
+  link: {
+    color: 'white',
+    fontSize: 42,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    backgroundColor: 'rgba(0,0,0,.5)',
+    textDecorationLine: 'underline',
+    padding: 4,
+  },
+  button: {
+    height: 60,
+    borderRadius: 20,
+    justifyContent: 'center',
+    backgroundColor: 'rgba(0, 0, 0, .75)',
+    padding: 6
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    padding: 4,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+  }
+})
